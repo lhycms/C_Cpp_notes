859aed19cc9956dd79de9517641d5b68c25ad67e
Uper <1404293933@qq.com>
2022-03-26 15:01:05 +0800

    多维数组

--- c++_顺序容器/code/test.cpp
+++ c++_顺序容器/code/test.cpp
@@ -1,18 +1,45 @@
 #include <iostream>
 
+void output_version1(int (&a)[3][4]);
+void output_version2(int (&a)[3][4]);
+void output_version3(int (&a)[3][4]);
+
+int ia[3][4] = { {0, 1, 2, 3},
+                 {4, 5, 6, 7},
+                 {8, 9, 10, 11} };
 
 int main() {
-    int ia[3][4];
+    output_version2(ia);
+    return EXIT_SUCCESS;
+}
 
-    // using int_array = int[4];
-    typedef int int_array[4];
+void output_version1(int (&a)[3][4]) {
+    for (int (&row)[4]: a) {
+        for (int (&col): row) {
+            std::cout << col << ' ';
+        }
+        std::cout << std::endl;
+    }    
+}
 
-    for (int_array *p = ia; p != ia+3; p++) {
-        for (int *q = *p; q != *p+4; ++q) {
-            std::cout << *q << ' ';
+void output_version2(int (&a)[3][4]) {
+    int rowCnt = 3;
+    int colCnt = 4;
+
+    for (int i=0; i<rowCnt; i++) {
+        for (int j=0; j<colCnt; j++) {
+            std::cout << a[i][j] << ' ';
         }
         std::cout << std::endl;
     }
+}
 
-    return 0;
+
+void output_version3(int (&a)[3][4]) {
+    for (int (*row)[4]=std::begin(a); row<a+3; row++) {
+        for (int *col=std::begin(*row); col<(*row)+4; col++) {
+            std::cout << *col << ' ';
+        }
+        std::cout << std::endl;
+    }
 }
