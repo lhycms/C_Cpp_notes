bfb33ed883aae08c5bc278acb552cb1b2a4bfb27
Uper <1404293933@qq.com>
2022-03-26 11:36:36 +0800

    多维数组

--- c++_顺序容器/code/test.cpp
+++ c++_顺序容器/code/test.cpp
@@ -1,13 +1,18 @@
 #include <iostream>
 
-int main(int argc, char **argv) {
-    int arr[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-    
-    int *e = &arr[10];
 
-    for (int *b=arr; b!=e; b++) {
-        std::cout << *b << std::endl;
+int main() {
+    int ia[3][4];
+
+    // using int_array = int[4];
+    typedef int int_array[4];
+
+    for (int_array *p = ia; p != ia+3; p++) {
+        for (int *q = *p; q != *p+4; ++q) {
+            std::cout << *q << ' ';
+        }
+        std::cout << std::endl;
     }
 
-    return EXIT_SUCCESS;
+    return 0;
 }
