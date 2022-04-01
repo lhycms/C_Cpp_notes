8f61a57c037d1f1c6ebeb5483226ef2bd4964b1f
Uper <1404293933@qq.com>
2022-04-01 14:31:43 +0800

    C++ make

---
+++ make/codes/function2.cpp
@@ -0,0 +1,15 @@
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: sch
+ * @Date: 2022-03-30 17:07:48
+ * @LastEditors: sch
+ * @LastEditTime: 2022-03-30 17:13:13
+ */
+#include <iostream>
+#include "functions.h"
+
+
+void print_hello() {
+    std::cout << "Hello world!\n";
+}
