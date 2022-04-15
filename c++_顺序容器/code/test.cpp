1169a8cec5ac399f14fe53dc1eed51985fd7ead9
Uper <1404293933@qq.com>
2022-04-15 20:10:19 +0800

    STL list function

--- c++_顺序容器/code/test.cpp
+++ c++_顺序容器/code/test.cpp
@@ -1,16 +1,38 @@
-// CPP program to show the implementation of list
-#include <bits/stdc++.h>
-
-
-// function for printing the elements in a list
-void showlist(std::list<int> &lst) {
-    for (std::vector<int>::iterator it=lst.begin(); it != lst.end(); it++)
-        std::cout << "\t" << (*it);
-    std::cout << std::endl;
-}
+#include <iostream>
+#include <list>
 
 
 int main() {
-    
+    // Declaring a list
+    std::list<int> lst{};
+
+    // using `assign()` to insert multiple numbers
+    // creates 3 occurences of "2"(int)
+    lst.assign(3, 2);
+
+    // initializing list iterator to beginning
+    std::list<int>::iterator it = lst.begin();
+
+    // iterator to point to 3rd position
+    std::advance(it, 2);
+
+    // using insert to insert 1 element at 3rd position
+    // inserts 5 at 3rd position
+    lst.insert(it, 5);
+
+    std::cout << "After first insert: \n";
+    for (auto &value: lst)
+        std::cout << value << "  ";
+    std::cout << std::endl;
+
+    // using insert to insert 2 element at 4th position
+    // inserts 2 occurences of 7 at 4th position
+    lst.insert(it, 2, 7);
+
+    std::cout << "After second insert:\n";
+    for (auto &value: lst)
+        std::cout << value << "  ";
+    std::cout << std::endl;
+
     return 0;
 }
