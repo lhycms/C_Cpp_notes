93f3d92ea32663e66cb995680b8be35de298d4c2
Uper <1404293933@qq.com>
2022-04-15 10:41:37 +0800

    STL: 找出vector中所有element

--- c++_顺序容器/code/test.cpp
+++ c++_顺序容器/code/test.cpp
@@ -1,23 +1,33 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
+#include <bits/stdc++.h>
+
+
+void find_all_key(std::vector<int>&, int&, std::vector<size_t>&);
 
 
 int main() {
-    std::vector<int> v = { 7, 3, 6, 2, 6};
-    int key = 6;
+    std::vector<int> ints_lst = { 10, 20, 30, 40 };
+    int key = 20;
+    std::vector<size_t> indexes_lst{};
 
-    std::vector<int>::iterator itr = std::find(v.begin(),
-                                            v.end(),
-                                            key);
+    find_all_key(ints_lst, key, indexes_lst);
 
-    if (itr != v.cend() ) {
-        std::cout << "Element present at index: "
-                << std::distance(v.begin(), itr)
-                << std::endl;
-    }                        
-    else
-        std::cout << "Not Found!!!\n";
+    for (auto &value: indexes_lst) {
+        std::cout << value << "  ";
+    }
+    std::cout << std::endl;
 
     return 0;
+}
+
+
+void find_all_key(std::vector<int> &ints_lst, int &key, std::vector<size_t> &indexes_lst) {
+    int tmp_index = 0;
+    std::vector<int>::iterator tmp_it_int = ints_lst.begin();
+
+    while ( tmp_it_int != ints_lst.end() ) {
+        tmp_it_int = std::find(tmp_it_int, ints_lst.end(), key);
+        tmp_index = std::distance(ints_lst.begin(), tmp_it_int);
+        indexes_lst.push_back(tmp_index);
+        tmp_it_int += 1;
+    }
 }
