edca73c8dc693567fe13f6e06c845b435f79f9dc
Uper <1404293933@qq.com>
2022-04-15 18:05:20 +0800

    deque

--- c++_顺序容器/code/test.cpp
+++ c++_顺序容器/code/test.cpp
@@ -1,33 +1,16 @@
+// CPP program to show the implementation of list
 #include <bits/stdc++.h>
 
 
-void find_all_key(std::vector<int>&, int&, std::vector<size_t>&);
-
-
-int main() {
-    std::vector<int> ints_lst = { 10, 20, 30, 40 };
-    int key = 20;
-    std::vector<size_t> indexes_lst{};
-
-    find_all_key(ints_lst, key, indexes_lst);
-
-    for (auto &value: indexes_lst) {
-        std::cout << value << "  ";
-    }
+// function for printing the elements in a list
+void showlist(std::list<int> &lst) {
+    for (std::vector<int>::iterator it=lst.begin(); it != lst.end(); it++)
+        std::cout << "\t" << (*it);
     std::cout << std::endl;
-
-    return 0;
 }
 
 
-void find_all_key(std::vector<int> &ints_lst, int &key, std::vector<size_t> &indexes_lst) {
-    int tmp_index = 0;
-    std::vector<int>::iterator tmp_it_int = ints_lst.begin();
-
-    while ( tmp_it_int != ints_lst.end() ) {
-        tmp_it_int = std::find(tmp_it_int, ints_lst.end(), key);
-        tmp_index = std::distance(ints_lst.begin(), tmp_it_int);
-        indexes_lst.push_back(tmp_index);
-        tmp_it_int += 1;
-    }
+int main() {
+    
+    return 0;
 }
