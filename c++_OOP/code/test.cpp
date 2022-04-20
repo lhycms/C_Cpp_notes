c64c402801dff960927ff12f4aa07dc67ab1246e
Uper <1404293933@qq.com>
2022-04-20 16:37:39 +0800

    运算符重载 +=

--- c++_OOP/code/test.cpp
+++ c++_OOP/code/test.cpp
@@ -1,36 +1,83 @@
 #include <bits/stdc++.h>
 
 
-class Instrument {
+/*
+Part I
+------
+    `YouTubeChannel` Definition
+*/
+class YouTubeChannel {
 public:
-    virtual void make_sound() = 0;
+    // Constructor Function
+    YouTubeChannel() = default;
+    YouTubeChannel(std::string name, unsigned int subscribers_count);
+
+    std::string get_name();
+    unsigned int get_subscribers_count();
+
+
+private:
+    std::string Name;
+    unsigned int SubscribersCount;
 };
 
 
-class Accordion: public Instrument {
-public:
-    void make_sound() {
-        std::cout << "Playing Accordion...\n";
-    }
-};
+// 成员函数
+YouTubeChannel::YouTubeChannel(std::string name, unsigned int subscribers_count): Name(name), SubscribersCount(subscribers_count) { };
+
+// 成员函数
+std::string YouTubeChannel::get_name() {
+    return Name;
+}
+
+// 成员函数
+unsigned int YouTubeChannel::get_subscribers_count() {
+    return SubscribersCount;
+}
+
+// 运算符`<<`重载 -- 针对` YouTubeChannel`
+std::ostream& operator<<(std::ostream& COUT, YouTubeChannel &yt_channel) {
+    std::cout << yt_channel.get_name() << std::endl;
+    std::cout << yt_channel.get_subscribers_count() << std::endl;
+    return COUT;
+}
 
 
-class Piano: public Instrument {
+
+/*
+Part II
+-------
+    MyCollection Definition
+*/
+class MyCollection {
 public:
-    void make_sound() {
-        std::cout << "Playing Piano...\n";
-    }
+    std::vector<YouTubeChannel> my_channels;
+
+    MyCollection() = default;
+
+    void operator+=(YouTubeChannel &yt_channel);
 };
+
+// 成员函数
+void MyCollection::operator+=(YouTubeChannel &yt_channel) {
+    my_channels.push_back(yt_channel);
+}
+
+// 运算符重载
+std::ostream& operator<<(std::ostream &COUT, MyCollection &my_collection) {
+    for (auto &yt_channel: my_collection.my_channels) 
+        COUT << yt_channel;
+    return COUT;
+}
 
 
 int main() {
-    Instrument* ptr_accordion = new Accordion();
-    Instrument* ptr_piano = new Piano();
+    YouTubeChannel* ptr_yt_channel = new YouTubeChannel("Programming", 24);
 
-    Instrument* instruments_lst[] = {ptr_accordion, ptr_piano};
-
-    for (auto ptr: instruments_lst)
-        ptr->make_sound();
+    MyCollection *my_collection = new MyCollection();
+    *my_collection += *ptr_yt_channel;
+    *my_collection += *ptr_yt_channel;
+    std::cout << *my_collection << std::endl;
 
     return 0;
 }
