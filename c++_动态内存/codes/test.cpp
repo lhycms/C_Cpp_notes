e5fb8257ba0b06e6476fa66cc06301c1911ba8ba
Uper <1404293933@qq.com>
2022-04-22 10:22:41 +0800

    code: StrLst

--- c++_动态内存/codes/test.cpp
+++ c++_动态内存/codes/test.cpp
@@ -2,10 +2,9 @@
 #include <exception>
 
 
-struct MyException: public std::exception
-{
+struct MyException: public std::exception {
     const char* what() const throw() {
-        return "Overflow!\n";
+        return "Overflow!";
     }
 };
 
@@ -16,38 +15,44 @@ public:
 
     // 构造函数
     StrLst() = default;
-    StrLst(std::initializer_list<std::string> &lst);
+    StrLst(std::initializer_list<std::string> lst);
 
-
-    // empty(), size()
-    bool empty() const { return ptr_data->empty(); }
-    size_type size() const { return ptr_data->size(); }
+    // size(), empty()
+    const size_type size() { return ptr_data->size(); }
+    const bool empty() { return ptr_data->empty(); }
 
     // 访问元素
     std::string& front();
     std::string& back();
 
     // 添加、删除元素
-    void push_back(const std::string &str);
+    void push_back(std::string &str);
     void pop_back();
 
-    // 友元函数
-    friend std::ostream& operator<<(std::ostream &COUT, StrLst &str_lst);
+    // 友元函数 -- 运算符重载
+    friend std::ostream& operator<<(std::ostream& COUT, StrLst &str_lst);
 
 private:
-    // ptr_data 是一个指向 `vector<std::string>` 的指针
     std::shared_ptr< std::vector<std::string> > ptr_data;
     void check(size_type i);
 };
 
-
-// 构造函数
-StrLst::StrLst(std::initializer_list<std::string> &lst): ptr_data(std::make_shared< std::vector<std::string> >(lst)) {};
-
-// check 函数
+// private 成员函数
 void StrLst::check(size_type i) {
     if ( i > ptr_data->size() )
         throw MyException();
+}
+
+// 成员函数 -- 构造函数
+StrLst::StrLst(std::initializer_list<std::string> lst): ptr_data(std::make_shared< std::vector<std::string> >(lst)) { };
+
+// 友元函数 -- 运算符重载
+std::ostream& operator<<(std::ostream& COUT, StrLst &str_lst) {
+    for (auto it = str_lst.ptr_data->begin();
+            it != str_lst.ptr_data->end(); it++) {
+        std::cout << (*it) << '\t';
+    }
+    return COUT;
 }
 
 // 成员函数 -- 访问元素
@@ -61,8 +66,9 @@ std::string& StrLst::back() {
     return ptr_data->back();
 }
 
+
 // 成员函数 -- 添加、删除元素
-void StrLst::push_back(const std::string &str) {
+void StrLst::push_back(std::string &str) {
     check(0);
     ptr_data->push_back(str);
 }
@@ -73,28 +79,18 @@ void StrLst::pop_back() {
 }
 
 
-// 运算符重载 -- `<<`
-std::ostream& operator<<(std::ostream& COUT, StrLst &str_lst) {
-    for (auto it=str_lst.ptr_data->begin();
-        it != str_lst.ptr_data->end(); it++) {
-        COUT << *it << "\t";
-    }
-    return COUT;
-}
-
-
 int main() {
-    std::initializer_list<std::string> lst{"Liu", "Han", "Yu"};
-    StrLst strlst_1(lst);
-    StrLst strlst_2 = strlst_1;
+    StrLst str_lst_1({"Liu", "Han", "Yu"});
+    StrLst str_lst_2 = str_lst_1;
 
-    std::cout << "初始化:\n";
-    std::cout << strlst_1 << std::endl;
-    std::cout << strlst_2 << std::endl;
+    std::cout << "Initially:\n";
+    std::cout << str_lst_1 << std::endl;
+    std::cout << str_lst_2 << std::endl;
 
-    std::cout << "strlst_1 删除最后一个元素: \n";
-    strlst_1.pop_back();
-    std::cout << strlst_1 << std::endl;
-    std::cout << strlst_2 << std::endl;
+    str_lst_1.pop_back();
+    std::cout << "\nNow:\n";
+    std::cout << str_lst_1 << std::endl;
+    std::cout << str_lst_2 << std::endl;
+
     return 0;
 }
