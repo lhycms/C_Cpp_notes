0a8cb8e2bccc5fb2be5d52b9f82bb7c32f1d710e
Uper <1404293933@qq.com>
2022-04-29 15:02:45 +0800

    链接库 -- 1.Basic.md

---
+++ c++链接库/codes/functions.cpp
@@ -0,0 +1,14 @@
+/*
+ * @Author: your name
+ * @Date: 2022-04-29 14:39:21
+ * @LastEditTime: 2022-04-29 14:44:12
+ * @LastEditors: Please set LastEditors
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: /C_C++/c++链接库/codes/functions.cpp
+ */
+#include "functions.h"
+
+
+float multiply(float value_1, float value_2) {
+    return value_1 * value_2;
+}
---
+++ c++链接库/codes/functions.h
@@ -0,0 +1,15 @@
+/*
+ * @Author: your name
+ * @Date: 2022-04-29 14:39:12
+ * @LastEditTime: 2022-04-29 14:40:14
+ * @LastEditors: Please set LastEditors
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: /C_C++/c++链接库/codes/functions.h
+ */
+
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+float multiply(float value_1, float value_2);
+
+#endif
