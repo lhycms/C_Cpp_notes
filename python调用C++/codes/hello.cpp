d7ab0f657d28303bbdb8e3c59ca21da4fd97e01b
Uper <1404293933@qq.com>
2022-04-29 14:35:21 +0800

    链接库 1.md

--- python调用C++/codes/hello.cpp
+++ python调用C++/codes/hello.cpp
@@ -1,3 +1,11 @@
+/*
+ * @Descripttion: 
+ * @version: 
+ * @Author: sch
+ * @Date: 2022-04-22 15:06:01
+ * @LastEditors: sch
+ * @LastEditTime: 2022-04-22 15:29:51
+ */
 char const* greet()
 {
    return "hello, world";
