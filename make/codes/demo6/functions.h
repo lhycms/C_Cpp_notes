bc70810f0941419974def5066e95568abd580577
Uper <1404293933@qq.com>
2022-05-01 09:49:30 +0800

    make -- implicit rules, variables

---
+++ make/codes/demo6/functions.h
@@ -0,0 +1,15 @@
+/*
+ * @Author: your name
+ * @Date: 2022-04-30 17:11:07
+ * @LastEditTime: 2022-04-30 17:14:32
+ * @LastEditors: Please set LastEditors
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: /C_C++/make/codes/test/functions.h
+ */
+
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+float multiply(float value_1, float value_2);
+
+#endif
---
+++ make/codes/demo7/functions.cpp
@@ -0,0 +1,14 @@
+/*
+ * @Author: your name
+ * @Date: 2022-05-01 09:35:06
+ * @LastEditTime: 2022-05-01 09:36:31
+ * @LastEditors: Please set LastEditors
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: /C_C++/make/codes/test/functions.cpp
+ */
+#include "functions.h"
+
+
+float multiply(float value_1, float value_2) {
+    return value_1 * value_2;
+}
