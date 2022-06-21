29cd630e4d7d81deef0908835c07742bfea04582
Uper <41718895+Hyliu-BUAA@users.noreply.github.com>
2022-06-21 17:56:38 +0800

    Sort 3d points

--- C++_拷贝控制/code/test.cpp
+++ C++_拷贝控制/code/test.cpp
@@ -1,85 +1,46 @@
 #include <iostream>
-#include <cstring>
 
 
 
-class String {
+class Interval {
+
 private:
-    char *s;
-    size_t size;
-
+    int x, y;
 
 public:
-    // Constructor
-    String() = default;
-    String(const char*);
+    // Constructor function
+    Interval() = default;
+    Interval(int x_value, int y_value):
+                            x(x_value),
+                            y(y_value) {
+        std::cout << "Calling constructor function...\n";
+    }
 
-    // Copy Constructor
-    String(const String&);
 
-    // Copy-assignment operator
-    String& operator=(const String&);
+    // Copy Constructor function
+    Interval(const Interval &i):
+                            x(i.x),
+                            y(i.y) {
+        std::cout << "Calling copy constructor function...\n";
+    }
 
-    // Destructor
-    ~String() { delete [] s; }
 
-    // member function
-    void print();
+    // Assignment Operator
+    Interval& operator=(const Interval &i) {
+        x = i.x;
+        y = i.y;
+        std::cout << "Calling assignment operator...\n";
 
-    // member function
-    void change(const char *);
+        return *this;
+    }
 
 };
 
 
-// Constructor 
-String::String(const char *c) {
-    size = strlen(c);
-    s = new char[size + 1];
-    strcpy(s, c);
-}
-
-// Copy Constructor
-String::String(const String &old_string) {
-    size = old_string.size;
-    s = new char[size + 1];
-    strcpy(s, old_string.s);
-}
-
-// Copy-assignment Operator
-String& String::operator=(const String &old_string) {
-    size = old_string.size;
-    s = new char[size + 1];
-    strcpy(s, old_string.s);
-    
-    return *this;
-}
-
-// member function 
-void String::print() {
-    std::cout << "s = " << s << std::endl;
-}
-
-// member function 
-void String::change(const char *c) {
-    delete [] s;
-    size = strlen(c);
-    s = new char[size + 1];
-    strcpy(s, c);
-}
-
-
 int main() {
-    String string_1 = String("Liu Hanyu");
-    String string_2;
-    string_2 = string_1;
-
-    string_1.print();
-    string_2.print();
-
-    string_2.change("Zhao Tong");
-    string_1.print();
-    string_2.print();
+    Interval interval = {1, 2};
+    Interval interval_2 = interval;
+    interval_2 = interval;
 
     return 0;
 }
--- c++_STL/code/test.cpp
+++ c++_STL/code/test.cpp
@@ -1,94 +1,74 @@
-/*
- * @Author: Uper 41718895+Hyliu-BUAA@users.noreply.github.com
- * @Date: 2022-06-04 17:46:29
- * @LastEditors: Uper 41718895+Hyliu-BUAA@users.noreply.github.com
- * @LastEditTime: 2022-06-10 10:39:14
- * @FilePath: /C_C++/c++_STL/code/test.cpp
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
 #include <iostream>
 #include <algorithm>
+#include <cmath>
+#include <initializer_list>
 
 
-class Interval
-{
+class Coordination {
 private:
-    int x, y;
+    int x, y, z;
 
 public:
-    // Constructor function
-    Interval() = default;
-    Interval(int x_value, int y_value) : 
-                        x(x_value),
-                        y(y_value)  {
-        std::cout << "Calling constructor...\n";
+    // Constructor 
+    Coordination() = default;
+
+    // Constructor
+    Coordination(int x_value, int y_value, int z_value):
+                        x(x_value), y(y_value), z(z_value)
+    {   }
+
+
+    // Copy Constructor
+    Coordination(const Coordination& coordination) {
+        x = coordination.x;
+        y = coordination.y;
+        z = coordination.z;
     }
-    
 
-    // Copy Function
-    Interval(const Interval &i) {
-        //std::cout << "Calling copy function...\n";
-        x = i.x;
-        y = i.y;
-    }
-
-
-    // Assignment Operator
-    Interval& operator=(const Interval &i) {
-        //std::cout << "Calling assignment operator...\n";
-        x = i.x;
-        y = i.y;
+    // Overloading copy assignment operator
+    Coordination& operator=(Coordination &coordination) {
+        x = coordination.x;
+        y = coordination.y;
+        z = coordination.z;
 
         return *this;
     }
+
+    // Overload `<<`
+    friend std::ostream& operator<<(std::ostream&, Coordination&);
     
-
-    // Friend function : The `cout` overloaded for Interval class
-    friend std::ostream& operator<<(std::ostream &COUT, Interval &i);
-
-    // Friend function : Compare function
-    friend bool compareInterval(Interval&, Interval&);
+    // Compare
+    friend bool compareCoordination(Coordination&, Coordination&);
 };
 
 
-// Friend function : The `cout` overloaded for Interval class
-std::ostream& operator<<(std::ostream &COUT, Interval &i) {
-    COUT << "\t" << "[" << i.x <<  ", " << i.y << "]";
-    COUT << std::endl;
-
+// Overload `<<`
+std::ostream& operator<<(std::ostream& COUT, Coordination &c) {
+    std::cout << "[" << c.x << ", " << c.y << ", " << c.z << "]" << std::endl;
     return COUT;
 }
 
 
-// show intervals_lst
-void show_intervals(Interval intervals_lst[], int size) {
-    for (int i = 0; i < size; i++) {
-        std::cout << intervals_lst[i];
+// Compare
+bool compareCoordination(Coordination &c1, Coordination &c2) {
+    float distance_1 = pow(c1.x, 2) + pow(c1.y, 2) + pow(c1.z, 2);
+    float distance_2 = pow(c2.x, 2) + pow(c2.y, 2) + pow(c2.z, 2);
+    return distance_1 < distance_2;
+}
+
+// show coordinations_lst
+void show(Coordination cs_lst[], int cs_size) {
+    for (int i=0; i<cs_size; i++) {
+        std::cout << cs_lst[i];
     }
 }
 
 
-// Compare function: 作为 `std::sort(<startaddress>, <endaddress>, <function_name>)` 的第三个函数
-// 升序排列
-bool compareInterval(Interval &i1, Interval &i2) {
-    return ( i1.x < i2.x );
-}
-
-
+// Driver Code
 int main() {
-    Interval intervals_lst[] = { { 6, 8 }, { 1, 9 }, { 2, 4 }, { 4, 7 } };
-    int size = sizeof(intervals_lst) / sizeof(intervals_lst[0]);
-
-    // output intervals_lst
-    std::cout << "Intervals_lst before sorting:\n";
-    show_intervals(intervals_lst, size);
-
-    // sort
-    std::sort(intervals_lst, intervals_lst + size, compareInterval);
-
-    // output intervals_lst
-    std::cout << "Intervals_lst after sorting:\n";
-    show_intervals(intervals_lst, size);
+    Coordination cs_lst[] = { {9,2,3}, {7,7,7}, {6, 8, 10} };
+    int cs_lst_size = sizeof(cs_lst) / sizeof(cs_lst[0]);
+    show(cs_lst, cs_lst_size);
 
     return 0;
 }
