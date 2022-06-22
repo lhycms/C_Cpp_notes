11db86987231de7e1298b14ae6798959123452e5
Uper <41718895+Hyliu-BUAA@users.noreply.github.com>
2022-06-22 21:01:34 +0800

    STL -- std::bsearch()

--- c++_STL/code/test.cpp
+++ c++_STL/code/test.cpp
@@ -1,39 +1,42 @@
-/*
-CPP program to implement `binary search` in STL 
-*/
-#include <iostream>
-#include <algorithm>
+// Cpp Program to implement `std::bsearch()`
+#include <bits/stdc++.h>
 
 
-void show(int array[], int array_size) {
-    for (int i=0; i<array_size; i++) {
-        std::cout << array[i] << "\t";
-    }
-    std::cout << std::endl;
+// Binary predicate
+int compare(const void *ap, const void *bp) {
+    // Typecasting
+    const int *a = (int*)ap;
+    const int *b = (int*)bp;
+
+    if (*a < *b)
+        return -1;
+    else if (*a > *b)
+        return 1;
+    else 
+        return 0;
 }
 
 
+// Driver code
 int main() {
-    int array[] = { 1, 5, 8, 9, 6, 7, 3, 4, 2, 0 };
-    int array_size = sizeof(array, array_size);
-    std::cout << "The array is: \n";
-    show(array, array_size);
+    // Given array
+    int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
 
-    // Step 1. Sort the array. 
-    std::cout << "Step 1, Sort the array: \n";
-    std::sort(array, array + array_size);
-    show(array, array_size);
+    // Size of array
+    int arr_size = sizeof(arr) / sizeof(arr[0]);
 
-    // Step 2. Search for 2, 10
-    if ( std::binary_search(array, array+array_size, 2) )
-        std::cout << "Element is found in array.\n";
+    // Element to be found
+    int key1 = 4;
+
+    // Calling `std::bsearch()`
+    // Typecasting the returned pointer to int
+    int *p1 = (int*)std::bsearch(&key1, arr, arr_size, sizeof(arr[0]), compare);
+
+    // If non-zero value is returned, key is found
+    if (p1)
+        std::cout << key1 << " found at position " << (p1 - arr) << std::endl;
     else
-        std::cout << "Element is not found in array.\n";
-
-    if ( std::binary_search(array, array+array_size, 10) )
-        std::cout << "Element is found in array.\n";
-    else
-        std::cout << "Element is found in array.\n";
+        std::cout << key1 << " not found.\n";
 
     return 0;
 }
