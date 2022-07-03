fb95fe0646229982161e71db6b627f1e8602e9df
Uper <41718895+Hyliu-BUAA@users.noreply.github.com>
2022-07-03 15:09:54 +0800

    initializer list constructor

--- c++_STL/code/test.cpp
+++ c++_STL/code/test.cpp
@@ -1,36 +1,36 @@
 #include <iostream>
+#include <initializer_list>
 
 
-class Point {
+class Coordination {
 private:
-    int x, y;
+    double x, y, z;
 
 public:
-    // Constructor
-    Point(int x_value=0, int y_value=0) : x(x_value), y(y_value)
-    {   }
-    /*
-        The above use of Initializer list is optional as the
-        constructor can also be written as:
+    // Constructor 1
+    Coordination() = default;
 
-        Point(int i = 0, int j = 0) {
-            x = i;
-            y = j;
-        }
+    // Constructor 2
+    Coordination(double x_value, double y_value, double z_value) {
+        std::cout << "Calling constructor...\n";
+        x = x_value;
+        y = y_value;
+        z = z_value;
+    }
 
-    */
-
-   // member function 1
-   int getX() const {return x;}
-
-   // member function 2
-   int getY() const {return y;}
+    // Initializer list constructor
+    Coordination(std::initializer_list<double> lst) {
+        std::cout << "Calling initializer list constructor...\n";
+        x = *(lst.begin());     // x = 11
+        y = *(lst.begin()+1);   // y = 22
+        z = *(lst.begin()+2);   // z = 33
+    }
 };
 
 
 int main() {
-    Point t1(10, 15);
-    std::cout << "x = " << t1.getX() << ", ";
-    std::cout << "y = " << t1.getY() << std::endl;
+    Coordination c_1 = {11,22,33};
+    Coordination c_2(4,5,6);
+
     return 0;
 }
