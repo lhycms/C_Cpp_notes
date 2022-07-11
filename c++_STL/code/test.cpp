3e8f0af41062e7abeb85ac42520f1a0bbf0a4e79
Uper <41718895+Hyliu-BUAA@users.noreply.github.com>
2022-07-11 23:40:30 +0800

    cannot convert const_value to mutable_value && cannot initialize int* with void*

--- c++_STL/code/test.cpp
+++ c++_STL/code/test.cpp
@@ -1,81 +1,33 @@
 #include <iostream>
-#include <cmath>
 #include <algorithm>
-#include <initializer_list>
 
 
+int compare(const void *p_1, const void *p_2) {
+    const int *p1 = (int*)(p_1);
+    const int *p2 = (int*)(p_2);
 
-class Coordination {
-private:
-    double x, y, z;
-
-public:
-    // Constructor
-    Coordination(double x_value, double y_value, double z_value) {
-        std::cout << "Constructor is running...\n";
-        x = x_value;
-        y = y_value;
-        z = z_value;
-    }
-
-    // Initializer list constructor
-    Coordination(std::initializer_list<double> lst) {
-        std::cout << "Initializer list constructor is running...\n";
-        x = *(lst.begin());
-        y = *(lst.begin() + 1);
-        z = *(lst.begin() + 2);
-    }
-
-
-    // member function -- overload the `function-call operator()` (调用运算符)
-    double operator()(int idx_direction) const {
-        if (idx_direction == 0)
-            return x;
-        else if (idx_direction == 1)
-            return y;
-        else
-            return z;
-    }
-
-
-    // friend function -- overloading the `operator<<`
-    friend std::ostream& operator<<(std::ostream &COUT, Coordination &c1);
-
-    // friend function -- used in `std::sort()` function
-    friend bool compare_coordinations(Coordination &c1, Coordination &c2);
-};
-
-
-// friend function 1
-std::ostream& operator<<(std::ostream &COUT, Coordination &c1) {
-    double distance = sqrt(pow(c1.x, 2) + pow(c1.y, 2) + pow(c1.z, 2));
-    COUT << "[" << c1.x << ", " << c1.y << ", " << c1.z << "]\t";
-    COUT << "Distance = " << distance << std::endl;
-
-    return COUT;
-}
-
-
-// friend function 2
-bool compare_coordinations(Coordination &c1, Coordination &c2) {
-    double distance_1 = pow(c1.x, 2) + pow(c1.y, 2) + pow(c1.z, 2);
-    double distance_2 = pow(c2.x, 2) + pow(c2.y, 2) + pow(c2.z, 2);
-    
-    return distance_1 >= distance_2;
+    if (*p1 > *p2)
+        return 1;
+    else if (*p1 < *p2)
+        return -1;
+    else
+        return 0;
 }
 
 
 int main() {
-    Coordination c_lst[] = { {1, 2, 3}, 
-                             {9, 5, 4},
-                             {4, 5, 6}
-                            };
-    int size = sizeof(c_lst) / sizeof(c_lst[0]);
+    int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+    int size = sizeof(arr) / sizeof(arr[0]);
+    int target = 8;
 
-    std::sort(c_lst, c_lst + size, compare_coordinations);
+    std::sort(arr, arr+size);
 
-    for (auto c_value: c_lst)
-        std::cout << c_value;
-    
+    int *p1 = (int*)std::bsearch(&target, arr, size, sizeof(arr[0]), compare);
+
+    if (p1)
+        std::cout << "Found at " << (p1 - arr) << std::endl;
+    else
+        std::cout << "Not found.\n";
+
     return 0;
 }
