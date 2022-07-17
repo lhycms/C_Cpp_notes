5d7bd921b1980143fb2543ddec7f7c817719f199
Uper <41718895+Hyliu-BUAA@users.noreply.github.com>
2022-07-17 22:09:51 +0800

    combinatoric iterator

--- python_libs/itertools_lib/code/test.py
+++ python_libs/itertools_lib/code/test.py
@@ -5,17 +5,17 @@ lst = [1, 2, 3, 4]
 count_2 = 0
 count_3 = 0
 
-for item in itertools.permutations(lst, 2):
-    count_2 += 1
+for item in itertools.combinations(lst, 2):
     print(item)
+    count_2 += 1
 
 print('*' * 40)
 
-for item in itertools.permutations(lst, 3):
-    count_3 += 1
+for item in itertools.combinations(lst, 3):
     print(item)
+    count_3 += 1
 
-# count_2 = 4 * 3 = 12
+# count_2 = (4*3) / (1*2) = 6
 print("count_2 = {0}".format(count_2))
-# count_3 = 4 * 3 * 2 = 24 
+# count_3 = (4*3*2) / (1*2*3) = 4
 print("count_3 = {0}".format(count_3))
